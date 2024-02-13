b0da9373f791e1ed5342960160647866f5474fdd
UniverseArc <93486544+UniverseArc@users.noreply.github.com>
2024-02-13 21:44:50 +0300

    feature: TimeBar ready

--- src/App.tsx
+++ src/App.tsx
@@ -1,10 +1,14 @@
 import './App.css'
+import TimeBar from './components/TimeBar/TimeBarVariants/TimeBar'
+import { TimeBarsVariant } from './components/TimeBar/TimeBarVariants/types/TimeBars.types'
 
 function App() {
 
   return (
     <div>
-
+      <TimeBar variant={TimeBarsVariant.primary} time={{hours: 13, minutes: 15}}/>
+      <TimeBar variant={TimeBarsVariant.fullfiledRed} time={{hours: 13, minutes: 15}}/>
+      <TimeBar variant={TimeBarsVariant.outlinedRed} time={{hours: 13, minutes: 15}}/>
     </div>
   )
 }
