228e8404951ff6bb6407cd34d6f941fa2248c61a
UniverseArc <93486544+UniverseArc@users.noreply.github.com>
2024-02-27 10:47:43 +0300

    feat: apply-button-buildet

---
+++ src/components/ApplyButton/ApplyButton.tsx
@@ -0,0 +1,21 @@
+import { FC } from "react";
+import s from "./ApplyButton.module.css"
+import clsx from 'clsx';
+import { EApplyButtonStyles, IApplyButton } from "./types/ApplyButton.types";
+
+const ApplyButton: FC<IApplyButton> = ({ buttonStyle}) => {
+
+    const colorClasses = {
+        [EApplyButtonStyles.White]: { class: s.buttonApply_white, text: 'Apply' },
+        [EApplyButtonStyles.Green]: { class: s.buttonApply_green, text: 'Applied!' },
+        [EApplyButtonStyles.Red]: { class: s.buttonApply_red, text: 'Declined!' },
+    };
+
+    const { class: buttonClassName, text: buttonText } = colorClasses[buttonStyle] || { class: s.buttonApply, text: 'Apply' };
+
+    return (
+        <button className={clsx(s.buttonApply, buttonClassName)}>{buttonText}</button>
+    )
+};
+
+export { ApplyButton };
---
+++ src/components/ApplyButton/types/ApplyButton.types.ts
@@ -0,0 +1,10 @@
+export enum EApplyButtonStyles {
+    White = "white",
+    Green = "green",
+    Red = "red"
+}
+
+export interface IApplyButton {
+    buttonStyle: EApplyButtonStyles
+    buttonText: string
+}
